c8ab5e53fdf0e4ec735ed5d89fd8695b0087f90e
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2023-07-05 08:55:52 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,8 +14,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
+            'id' => 1,
             'name'  => 'Fahmi Fauzi Rahman',
             'email' => 'fahmifauzirahman@gmail.com',
+            'password'  => '123456',
+        ]);
+
+        User::create([
+            'id' => 2,
+            'name'  => 'Dzaki Dhafir Rahman',
+            'email' => 'dzakidhafirrahman@gmail.com',
             'password'  => '123456',
         ]);
     }
